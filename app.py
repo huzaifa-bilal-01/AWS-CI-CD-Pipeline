fbf473aa654a0740d8080622ee317fb2ddecc4ed
huzaifa-bilal-01 <huzaifabilal226@gmail.com>
2024-05-02 19:38:37 +0500

    changes in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! how are you and how are u doing :)'
+    return 'Hello, world! how are you and how are u:)'
 
 if __name__ == '__main__':
     app.run()
